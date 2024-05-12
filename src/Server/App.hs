67671bc602173f2bc4f520fb08328204307d8953
Mike Taylor <mike.charles.taylor@gmail.com>
2024-05-12 17:47:32 -0600

    refactoring

--- src/Server/App.hs
+++ src/Server/App.hs
@@ -1,9 +1,12 @@
+{-# LANGUAGE DataKinds #-}
 module Server.App
   ( app
   ) where
 
+import Network.Wai
 import Network.Wai.Middleware.RequestLogger
 import Servant
+import Servant.Server.Experimental.Auth
 import qualified Network.Wai as Wai
 import qualified Network.Wai.Handler.WebSockets as WaiWS
 import qualified Network.WebSockets as WS
@@ -18,12 +21,15 @@ import State
 
 app :: State -> Wai.Application
 app state req respond' =
-  let handler = websocketHandler state
-   in case WaiWS.websocketsApp WS.defaultConnectionOptions handler req of
+  case WaiWS.websocketsApp WS.defaultConnectionOptions handler req of
     Just response -> respond' response
-    Nothing -> logStdout (
-      serveWithContext
-      (Proxy :: Proxy API)
-      (authContext (configHost $ unStateConfig state) (unStateClientEnv state))
-      (server state)
-      ) req respond'
+    Nothing -> logStdout application req respond'
+  where
+    apiProxy :: Proxy API
+    apiProxy = Proxy
+    application :: Wai.Application
+    application = serveWithContext apiProxy authContext' (server state)
+    authContext' :: Context '[AuthHandler Request Auth]
+    authContext' = authContext configHost' (unStateClientEnv state)
+    configHost' = configHost $ unStateConfig state
+    handler = websocketHandler state
